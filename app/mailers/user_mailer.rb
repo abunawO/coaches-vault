a05656bebd05045cdad1e28147034f84e412f94f
Ose Abunaw <abunawose@gmail.com>
2026-02-17 13:59:56 -0500

    updated emial look

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   def email_verification(user)
     @user = user
     @verify_url = verify_email_url(token: user.signed_id(purpose: :email_verification, expires_in: 2.days))
-    mail to: user.email, subject: "Verify your email for MyVault"
+    mail to: user.email, subject: "Confirm your email for MyCoachVault"
   end
 
   def password_reset(user)
