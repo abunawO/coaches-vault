4e773093ddf3ca2be184ea14349d0603d5ddca31
Ose Abunaw <abunawose@gmail.com>
2026-02-18 15:21:15 -0500

    added ability to edit/delete comment

--- config/routes.rb
+++ config/routes.rb
@@ -72,7 +72,7 @@ Rails.application.routes.draw do
   get "/search", to: "search#index", as: :search
 
   resources :lessons, only: [] do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :update, :destroy]
   end
 
   resources :conversations, only: [:index, :show, :create, :destroy] do
